d66923aef7fe10976ce0c4ee2bb2a96b74630af6
jonaditommaso <jonaditommaso@gmail.com>
2025-04-08 22:19:34 +0200

    feat: get plan in account settings

--- src/features/settings/components/sections/plan.tsx
+++ src/features/settings/components/sections/plan.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { getCurrent } from "@/features/auth/queries";
+import capitalize from "@/utils/capitalize";
 
-const Plan = () => {
+const Plan = async () => {
+    const user = await getCurrent();
+
     return (
         <div className="flex items-center justify-between w-full">
-            <h2>Free</h2>
+            <h2>{capitalize(user?.prefs?.plan)}</h2>
             <Button variant='outline'>Ver planes</Button>
         </div>
     );
