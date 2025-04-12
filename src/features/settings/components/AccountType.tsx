c607882f5a226f39bb1a793a3a66df9a63ed2589
jonaditommaso <jonaditommaso@gmail.com>
2025-04-12 16:18:27 +0200

    feat: show correctly account type

--- src/features/settings/components/AccountType.tsx
+++ src/features/settings/components/AccountType.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { useGetMember } from "@/features/members/api/use-get-member";
+import { useCurrent } from "@/features/auth/api/use-current";
+
 
 const AccountType = () => {
-    const { data: member } = useGetMember();
+    const { data: user } = useCurrent();
 
     return (
         <div className="text-center">
             <p className="text-xl">Tipo de cuenta</p>
-            <p>{member?.role}</p>
+            <p>{user?.prefs.role === 'ADMIN' ? 'ADMIN' : 'CREATOR' }</p>
         </div>
     );
 }
