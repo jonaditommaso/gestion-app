29c5d7126925e29ba815549b12877abc54bb802a
jonaditommaso <jonaditommaso@gmail.com>
2025-04-13 20:16:41 +0200

    chore: pass getting profile picture logic to hook

---
+++ src/hooks/useProfilePicture.ts
@@ -0,0 +1,24 @@
+'use client'
+
+import { useGetImageProfile } from "@/features/settings/api/use-get-image-profile";
+import { useEffect, useState } from "react";
+
+export const useProfilePicture = () => {
+    const [imageUrl, setImageUrl] = useState<any>(undefined);
+    const {mutate: getImageProfile } = useGetImageProfile()
+
+
+    useEffect(() => {
+        getImageProfile(undefined, {
+            onSuccess: (blob) => {
+                const url = URL.createObjectURL(blob);
+                setImageUrl(url);
+            },
+            onError: (err) => {
+                console.error('No se pudo obtener la imagen:', err);
+            }
+            })
+    }, []);
+
+    return { imageUrl }
+}
