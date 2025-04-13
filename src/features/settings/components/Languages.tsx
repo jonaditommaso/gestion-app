cc20512e3b7028e834e5927d8fb86e97eb23d100
jonaditommaso <jonaditommaso@gmail.com>
2025-04-14 00:27:21 +0200

    feat: change language in settings

--- src/features/settings/components/Languages.tsx
+++ src/features/settings/components/Languages.tsx
@@ -1,13 +1,25 @@
+'use client'
+import { cn } from "@/lib/utils";
+import { useLocale } from "next-intl";
 import Image from "next/image";
 
 const Languages = () => {
+    const currentLocale = useLocale();
+
+    const changeLanguage = (locale: string) => {
+        if (locale === currentLocale) return;
+
+        document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=31536000`;
+        window.location.reload();
+    };
+
     return (
         <div>
             <p className="text-xl">Languages</p>
             <div className="flex gap-4 mt-4">
-                <Image src="/flags/ES-flag.svg" alt="Spanish flag" width={24} height={16} />
-                <Image src="/flags/US-flag.svg" alt="United States flag" width={22} height={16} className="transform scale-125" />
-                <Image src="/flags/IT-flag.svg" alt="Italian flag" width={24} height={16} />
+                <Image src="/flags/ES-flag.svg" alt="Spanish flag" width={24} height={16} className={cn("cursor-pointer", currentLocale !== 'es' ? 'grayscale' : '')} onClick={() => changeLanguage('es')} />
+                <Image src="/flags/US-flag.svg" alt="United States flag" width={22} height={16} className={cn("transform scale-125 cursor-pointer", currentLocale !== 'en' ? 'grayscale' : '')} onClick={() => changeLanguage('en')} />
+                <Image src="/flags/IT-flag.svg" alt="Italian flag" width={24} height={16} className={cn("cursor-pointer", currentLocale !== 'it' ? 'grayscale' : '')} onClick={() => changeLanguage('it')} />
             </div>
         </div>
     );
