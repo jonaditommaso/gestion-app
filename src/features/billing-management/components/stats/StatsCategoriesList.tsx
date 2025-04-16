fb882adfa71cba0e513b089ba2fe566a6895b17e
jonaditommaso <jonaditommaso@gmail.com>
2025-04-16 02:01:14 +0200

    fix: solve error giving key to categories in map function

--- src/features/billing-management/components/stats/StatsCategoriesList.tsx
+++ src/features/billing-management/components/stats/StatsCategoriesList.tsx
@@ -15,11 +15,11 @@ const StatsCategoriesList = ({ categoriesData }: StatsCategoriesListProps) => {
 
     return (
         <div className="min-w-[300px] w-[450px] md:pl-[45px] md:m-auto">
-            {categoriesData.map(element => {
+            {categoriesData.map((element, index) => {
                 const percentage = ((element.import / totalCount) * 100).toFixed(1);
 
                 return (
-                    <div className="flex justify-between mx-2 items-center border-[1px] p-2 w-[400px]">
+                    <div className="flex justify-between mx-2 items-center border-[1px] p-2 w-[400px]" key={index}>
                         <div className="flex items-center gap-2">
                             <Badge style={{ backgroundColor: element.fill, width: '55px' }}>{percentage}%</Badge>
                             <span>{element.category}</span>
