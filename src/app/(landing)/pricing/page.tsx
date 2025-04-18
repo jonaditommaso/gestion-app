50c05c1c777347777131c78c595c04793a26a72d
jonaditommaso <jonaditommaso@gmail.com>
2025-04-18 20:17:07 +0200

    feat: translate pricing title

--- src/app/(landing)/pricing/page.tsx
+++ src/app/(landing)/pricing/page.tsx
@@ -2,15 +2,17 @@ import { getCurrent } from '@/features/auth/queries';
 import PricingCard from '../../../features/landing/components/PricingCard';
 import { plans } from '../../../features/landing/plans';
 import { redirect } from 'next/navigation';
+import { getTranslations } from 'next-intl/server';
 
 const PricingView = async () => {
     const user = await getCurrent();
+    const t = await getTranslations('pricing')
 
     if(user) redirect('/');
 
     return (
         <div className='mt-20 flex flex-col items-center'>
-            <p className="text-4xl font-bold text-balance text-center">Elige el plan perfecto para tu equipo</p>
+            <p className="text-4xl font-bold text-balance text-center">{t('pricing-title')}</p>
             <div className="container flex gap-3 mt-16 justify-center p-1">
                 {plans.map(plan => (
                     <PricingCard
