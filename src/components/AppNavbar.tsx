c2f2c0fd4165f346f75dae8345534f8852565c82
jonaditommaso <jonaditommaso@gmail.com>
2025-04-18 22:05:15 +0200

    feat: add command search

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-// import SearchCommand from "@/components/SearchCommand";
+import SearchCommand from "@/components/SearchCommand";
 // import { Badge } from "@/components/ui/badge";
 // import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"; // DropdownMenuLabel, DropdownMenuSeparator,
 import UserButton from "@/features/auth/components/UserButton";
@@ -18,7 +18,7 @@ const AppNavbar = () => {
   if(pathname === '/login' || pathname === '/signup') return null; //check how to implement it in ssr, and more gral
 
   return ( //ml-10
-    <nav className={`border-b shadow-md fixed top-0 z-20 grid grid-cols-2 items-center w-full ${theme === 'dark' ? 'bg-[#212121]' : 'bg-white' }`}>
+    <nav className={`border-b shadow-md fixed top-0 z-20 grid grid-cols-3 items-center w-full ${theme === 'dark' ? 'bg-[#212121]' : 'bg-white' }`}>
       {/* <DropdownMenu>
         <DropdownMenuTrigger className="max-w-40 flex items-center gap-2 p-2 focus:outline-none">
           <Image src='/logo.svg' height={50} width={100} alt="logo" />
@@ -30,8 +30,7 @@ const AppNavbar = () => {
       </DropdownMenu> */}
       <p className="max-w-40 flex items-center gap-2 p-2">{user?.prefs?.company}</p>
 
-      {/* //! TODO: APPLY THIS SEARCH LATER and put grid-cols-3 in nav to restore style */}
-      {/* <SearchCommand /> */}
+      <SearchCommand />
       <div className="flex items-center justify-end p-1 mr-5 gap-4">
         <ToggleThemeMode />
         {/* <div className="relative cursor-pointer">
--- src/components/SearchCommand.tsx
+++ src/components/SearchCommand.tsx
@@ -1,11 +1,9 @@
 'use client'
 import {
-  Calculator,
-  Calendar,
-  CreditCard,
-  Settings,
-  Smile,
+  NotepadText,
+  ReceiptText,
   User,
+  UserRoundSearch,
 } from "lucide-react"
 
 import {
@@ -15,73 +13,107 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-  CommandSeparator,
   CommandShortcut,
+  //CommandSeparator,
 } from "@/components/ui/command"
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
+
+const items = [
+  { label: "billing", icon: <ReceiptText className="mr-2 h-4 w-4" />, path: "/billing-management", shortcut: "j" },
+  { label: "records", icon: <UserRoundSearch className="mr-2 h-4 w-4" />, path: "/records", shortcut: "r" },
+  { label: "workspaces", icon: <NotepadText className="mr-2 h-4 w-4" />, path: "/workspaces", shortcut: "w" },
+  { label: "profile", icon: <User className="mr-2 h-4 w-4" />, path: "/settings", shortcut: "p" },
+];
 
 const SearchCommand = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isNavigating, setIsNavigating] = useState(false);
+  const router = useRouter();
+  const t = useTranslations('app')
 
-    const handleInputClick = () => setIsOpen(true);
+  const handleInputClick = () => setIsOpen(true);
 
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleNavigation = (path: string) => {
+    setIsNavigating(true);
+    router.push(path);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    const handleKeydown = (event: KeyboardEvent) => {
+      // open search
+      if ((event.ctrlKey || event.metaKey) && event.key === "k") {
+        event.preventDefault();
+        setIsOpen((prev) => {
+          const newState = !prev;
+          if (!prev) {
+
+            // wait input mounted
+            setTimeout(() => {
+              inputRef.current?.focus();
+            }, 0);
+          }
+          return newState;
+        });
       }
+
+      // custom shortcuts for item
+      items.forEach(({ shortcut, path }) => {
+        if ((event.ctrlKey || event.metaKey) && event.key === shortcut) {
+          event.preventDefault();
+          setIsNavigating(true);
+          handleNavigation(path);
+        }
+      });
     };
 
-    useEffect(() => {
-      if (isOpen) {
-        document.addEventListener("mousedown", handleOutsideClick);
-      } else {
-        document.removeEventListener("mousedown", handleOutsideClick);
-      }
-      return () => {
-        document.removeEventListener("mousedown", handleOutsideClick);
-      };
-    }, [isOpen]);
+    document.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleKeydown);
+      setIsNavigating(false)
+    };
+  }, [isOpen]);
 
   return (
     <div className="flex p-2 ml-1 relative h-14" ref={containerRef}>
       <Command className="rounded-lg border shadow-sm md:min-w-[250px] lg:max-w-8">
-        <CommandInput placeholder="Buscar..." onClick={handleInputClick} />
+        <CommandInput
+          placeholder={t('search')}
+          onClick={handleInputClick}
+          ref={inputRef}
+          disabled={isNavigating}
+          autoFocus
+        />
         {isOpen && <div className="absolute top-full w-full bg-white shadow-md z-50">
 
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Suggestions">
-            <CommandItem>
-                <Calendar />
-                <span>Calendar</span>
-            </CommandItem>
-            <CommandItem>
-                <Smile />
-                <span>Search Emoji</span>
-            </CommandItem>
-            <CommandItem disabled>
-                <Calculator />
-                <span>Calculator</span>
-            </CommandItem>
-            </CommandGroup>
-            <CommandSeparator />
-            <CommandGroup heading="Settings">
-            <CommandItem>
-                <User />
-                <span>Profile</span>
-                <CommandShortcut>⌘P</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-                <CreditCard />
-                <span>Billing</span>
-                <CommandShortcut>⌘B</CommandShortcut>
-            </CommandItem>
-            <CommandItem>
-                <Settings />
-                <span>Settings</span>
-                <CommandShortcut>⌘S</CommandShortcut>
-            </CommandItem>
+          <CommandList className="bg-sidebar">
+            <CommandEmpty>{t('no-results-found')}</CommandEmpty>
+            {/*<CommandSeparator /> */}
+            <CommandGroup heading={t('commands')}>
+              {(items ?? []).map((item) => (
+                  <CommandItem
+                    key={item.label}
+                    onSelect={() => handleNavigation(item.path)}
+                  >
+                    {item.icon}
+                    <span>{t(item.label)}</span>
+                    <CommandShortcut>⌘{item.shortcut.toUpperCase()}</CommandShortcut>
+                  </CommandItem>
+                ))}
             </CommandGroup>
           </CommandList>
         </div>}
