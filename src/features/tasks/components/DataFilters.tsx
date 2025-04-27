71e87285b011c3cb90335aa1ffca1bf8773df555
jonaditommaso <jonaditommaso@gmail.com>
2025-04-27 15:13:44 +0200

    fix: correct error translating due date

--- src/features/tasks/components/DataFilters.tsx
+++ src/features/tasks/components/DataFilters.tsx
@@ -98,7 +98,7 @@ const DataFilters = () => {
                 </SelectContent>
             </Select>
             <CustomDatePicker
-                placeholder={t('due-date')}
+                placeholder={'due-date'}
                 className="h-8 w-full lg:w-auto"
                 value={dueDate ? new Date(dueDate) : undefined}
                 onChange={date => { setFilters({ dueDate: date ? date.toISOString() : null }) }}
