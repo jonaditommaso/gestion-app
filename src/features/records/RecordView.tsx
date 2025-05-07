827361ad69ee762a197fe0137fb8c8ddafb07b1f
jonaditommaso <jonaditommaso@gmail.com>
2025-05-07 20:17:36 +0200

    feat: show recovered data in record view

--- src/features/records/RecordView.tsx
+++ src/features/records/RecordView.tsx
@@ -3,13 +3,28 @@ import FadeLoader from "react-spinners/FadeLoader";
 import { useGetRecord } from "./api/use-get-record";
 
 const RecordView = () => {
-    const { data, isPending } = useGetRecord();
+    const { data: record, isPending } = useGetRecord();
 
     if(isPending) return <FadeLoader color="#999" width={3} className="mt-5" />
 
     return (
-        <>
-        </>
+        <div>
+            <div>
+                {record?.data.map((item, index) => {
+                    const parsed = JSON.parse(item) as Record<string, string>;
+
+                    return (
+                        <div key={index}>
+                        {Object.entries(parsed).map(([key, value]) => (
+                            <div key={key}>
+                                <strong>{key}:</strong> {value}
+                            </div>
+                        ))}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
 
