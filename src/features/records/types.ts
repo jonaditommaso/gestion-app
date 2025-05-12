cd23633b5f9c737d62ccf3d4014aaa8c8e7d5934
jonaditommaso <jonaditommaso@gmail.com>
2025-05-12 23:13:39 +0200

    chore: remove files without use about records

--- src/features/records/types.ts
+++ src/features/records/types.ts
@@ -1,12 +1,5 @@
 import { Models } from "node-appwrite"
 
-export type Payment = {
-  id: string
-  amount: number
-  cuil: string
-  email: string
-}
-
 export interface RecordsContextType {
   data: Models.DocumentList<Models.Document>,
   isPending: boolean
