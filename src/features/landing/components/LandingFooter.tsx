55a7551f6857c53b273e2340971c055b970671e8
jonaditommaso <jonaditommaso@gmail.com>
2025-09-06 16:35:54 +0200

    feat: show footer in pricing view

--- src/features/landing/components/LandingFooter.tsx
+++ src/features/landing/components/LandingFooter.tsx
@@ -12,7 +12,6 @@ const LandingFooter = async () => {
 
             <CTAFooter />
 
-            <hr className="h-1 w-full my-4 border-muted-foreground" />
 
             <div className="flex items-start justify-around w-full  my-10">
                 <div className="flex items-center gap-4">
@@ -35,17 +34,17 @@ const LandingFooter = async () => {
                         <p className="font-semibold text-xl">Resources</p>
                         <Link href='/quick-start' className="hover:underline">Quick start guides</Link>
                         <Link href='/product-docs' className="hover:underline">Product Documentation</Link>
-                        <Link href='/docs' className="hover:underline">Learn more</Link>
+                        {/* <Link href='/docs' className="hover:underline">Learn more</Link> */}
                         <Link href='/faq' className="hover:underline">{t('footer-faq')}</Link>
                     </div>
                     <div className="flex flex-col gap-2">
                         <p className="font-semibold text-xl">Company</p>
                         <Link href='/about' className="hover:underline">About us</Link>
-                        <Link href='/' className="hover:underline">Leadership</Link>
+                        {/* <Link href='/' className="hover:underline">Leadership</Link> */}
                         <Link href='/blog' className="hover:underline">Blog</Link>
                         <Link href='/contact' className="hover:underline">{t('footer-contact-us')}</Link>
                         <Link href='/terms' className="hover:underline">{t('footer-terms')}</Link>
-                        <Link href='/privacy' className="hover:underline">{'footer-privacy'}</Link>
+                        <Link href='/privacy' className="hover:underline">{t('footer-privacy')}</Link>
                     </div>
                 </div>
             </div>
