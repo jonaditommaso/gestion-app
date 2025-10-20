04ed2607815c7962f612ad3fc6ebbd637a97ce25
jonaditommaso <jonaditommaso@gmail.com>
2025-10-20 17:22:50 -0300

    chore: create custom hook to get custom roles permissions

--- src/features/roles/components/RolesTab.tsx
+++ src/features/roles/components/RolesTab.tsx
@@ -2,31 +2,16 @@
 
 import { RoleCard } from "./RoleCard"
 import RoleEditModal from "./RoleEditModal"
-import { useMemo, useState } from "react"
+import { useState } from "react"
 import type { RoleType, Permission } from "../constants"
-import {
-  ROLE_METADATA,
-  rolePermissions
-} from "../constants"
-import { useGetRolesPermissions } from "../api/use-get-role-permissions"
+import { ROLE_METADATA } from "../constants"
+import { useGetFinalRolesPermissions } from "../hooks/useGetFinalRolesPermissions"
 
 export function RolesTab() {
   const [selectedRole, setSelectedRole] = useState<{ role: RoleType; permissions: Permission[], $id?: string } | null>(null)
   const [isRoleDialogOpen, setIsRoleDialogOpen] = useState(false);
-  const { data: customRolePermissions } = useGetRolesPermissions();
 
-  const finalRolePermissions = useMemo(() => rolePermissions.map(defaultRole => {
-    const customConfig = customRolePermissions?.documents?.find(custom => custom.role === defaultRole.role);
-
-    if (customConfig) {
-      return {
-        role: customConfig.role as RoleType,
-        permissions: customConfig.permissions as Permission[],
-        $id: customConfig.$id
-      };
-    }
-    return defaultRole;
-  }), [customRolePermissions]);
+  const finalRolePermissions = useGetFinalRolesPermissions();
 
   const handleEdit = (roleData: { role: RoleType; permissions: Permission[], $id?: string }) => {
     setSelectedRole(roleData)
