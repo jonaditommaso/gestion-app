e691d49dc812c833e5adeb42aaa43d70ce397490
jonaditommaso <jonaditommaso@gmail.com>
2026-02-18 17:26:52 -0300

    chore: create hook to get user permissions easily

---
+++ src/features/roles/hooks/useCurrentUserPermissions.ts
@@ -0,0 +1,15 @@
+import { useCurrent } from "@/features/auth/api/use-current";
+import { Permission, RoleType } from "../constants";
+import { useGetFinalRolesPermissions } from "./useGetFinalRolesPermissions";
+
+export const useCurrentUserPermissions = () => {
+    const { data: user } = useCurrent();
+    const finalRolePermissions = useGetFinalRolesPermissions();
+
+    const role = (user?.prefs?.role as RoleType) ?? 'VIEWER';
+    const permissions = finalRolePermissions.find(r => r.role === role)?.permissions ?? [] as Permission[];
+
+    const hasPermission = (permission: Permission): boolean => permissions.includes(permission);
+
+    return { permissions, hasPermission };
+};
