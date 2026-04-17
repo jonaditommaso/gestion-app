441ce4a09b9fa20e55ceedbf75ae10e7f70f7ba2
jonaditommaso <jonaditommaso@gmail.com>
2026-04-17 18:04:15 -0300

    feat: add gmail integration to landing

--- src/app/(landing)/products/products-integrations.ts
+++ src/app/(landing)/products/products-integrations.ts
@@ -1,9 +1,27 @@
 export const productsIntegrations = [
     {
-        name: 'Slack',
-        logo: '/integrations/slack.png',
-        color: '#611f69',
-        bgColor: 'rgba(97, 31, 105, 0.1)'
+        name: 'Google Calendar',
+        logo: '/integrations/calendar.png',
+        color: '#34a853',
+        bgColor: 'rgba(52, 168, 83, 0.1)'
+    },
+    {
+        name: 'Google Meet',
+        logo: '/integrations/meet.png',
+        color: '#00897b',
+        bgColor: 'rgba(0, 137, 123, 0.1)'
+    },
+    {
+        name: 'Google Drive',
+        logo: '/integrations/drive.png',
+        color: '#4285f4',
+        bgColor: 'rgba(66, 133, 244, 0.1)'
+    },
+    {
+        name: 'Gmail',
+        logo: '/integrations/gmail.png',
+        color: '#d93025',
+        bgColor: 'rgba(217, 48, 37, 0.1)'
     },
     {
         name: 'Trello',
@@ -11,6 +29,19 @@ export const productsIntegrations = [
         color: '#0079bf',
         bgColor: 'rgba(0, 121, 191, 0.1)'
     },
+    {
+        name: 'GitHub',
+        logo: '/integrations/github.png',
+        color: '#24292e',
+        bgColor: 'rgba(36, 41, 46, 0.1)'
+    },
+    {
+        name: 'Slack',
+        logo: '/integrations/slack.png',
+        color: '#611f69',
+        bgColor: 'rgba(97, 31, 105, 0.1)'
+    },
+
     {
         name: 'Jira',
         logo: '/integrations/jira.png',
@@ -24,22 +55,10 @@ export const productsIntegrations = [
         bgColor: 'rgba(255, 224, 27, 0.15)'
     },
     {
-        name: 'Google Drive',
-        logo: '/integrations/drive.png',
-        color: '#4285f4',
-        bgColor: 'rgba(66, 133, 244, 0.1)'
-    },
-    {
         name: 'Dropbox',
         logo: '/integrations/dropbox.png',
         color: '#0061ff',
         bgColor: 'rgba(0, 97, 255, 0.1)'
-    },
-    {
-        name: 'GitHub',
-        logo: '/integrations/github.png',
-        color: '#24292e',
-        bgColor: 'rgba(36, 41, 46, 0.1)'
     },
     {
         name: 'Microsoft Teams',
@@ -48,27 +67,15 @@ export const productsIntegrations = [
         bgColor: 'rgba(98, 100, 167, 0.1)'
     },
     {
-        name: 'Google Calendar',
-        logo: '/integrations/calendar.png',
-        color: '#34a853',
-        bgColor: 'rgba(52, 168, 83, 0.1)'
-    },
-    {
-        name: 'Google Meet',
-        logo: '/integrations/meet.png',
-        color: '#00897b',
-        bgColor: 'rgba(0, 137, 123, 0.1)'
-    },
-    {
         name: 'Notion',
         logo: '/integrations/notion.png',
         color: '#000000',
         bgColor: 'rgba(0, 0, 0, 0.1)'
     },
-    {
-        name: 'Spotify',
-        logo: '/integrations/spotify.png',
-        color: '#1db954',
-        bgColor: 'rgba(29, 185, 84, 0.1)'
-    }
+    // {
+    //     name: 'Spotify',
+    //     logo: '/integrations/spotify.png',
+    //     color: '#1db954',
+    //     bgColor: 'rgba(29, 185, 84, 0.1)'
+    // }
 ]
